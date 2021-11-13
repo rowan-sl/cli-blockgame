f13f4fdecfd8ffc32700db162d07a548b3415c7f
Rowan S-L <rowan.with.cats@gmail.com>
2021-11-13 17:40:42 -0500

    fixed more flickering

--- code/utils/border_world.py
+++ code/utils/border_world.py
@@ -17,7 +17,7 @@ def border_world(area: List[List[T]], radius: int) -> List[List[T]]:
     new_area.append(void*radius + border * (len(area[0]) + 2) + void*radius)
 
     for y, line in enumerate(area):
-        print("e")
+        # print("e")
         new_area.append((void*radius + border + line + border + void*radius))
 
     new_area.append(void*radius + border * (len(area[0]) + 2) + void*radius)
--- code/utils/term_utils.py
+++
@@ -1,5 +0,0 @@
-def reset_cursor():
-    print("\033[H")
-
-def clear_screen():
-    print("\033[2J")
